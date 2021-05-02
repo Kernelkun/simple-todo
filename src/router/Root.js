b7cc7fe672e3dbc7d7ba56315334e0b2696f01fd
José Juan Gázquez <josejuan.gazquez@intelygenz.com>
2021-05-02 19:14:55 +0200

    New: add basic user context

--- src/router/Root.js
+++ src/router/Root.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
+import {UserContext} from '../components/App/App'
 import LoggedIn from './LoggedIn'
 import LoggedOut from './LoggedOut'
 
 const Root = () => {
-  const logged = false
+  const {logged} = useContext(UserContext)
 
   return (
     <Router>

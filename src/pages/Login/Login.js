baef90b0a3f658ebbe55b5c823bd7c9f7bdadb74
José Juan Gázquez <josejuan.gazquez@intelygenz.com>
2021-05-02 20:58:26 +0200

    New: add login form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Button, Grid, TextField} from '@material-ui/core'
 import useUser from '../../hooks/useUser'
+import {callToLoginEndpoint} from '../../services/auth'
 
 const Login = () => {
   const {setUser} = useUser()
+  const [formData, setFormData] = useState({user: '', password: ''})
 
-  const handleLogin = () => setUser({logged: true})
+  const handleLogin = () => {
+    const {logged} = callToLoginEndpoint({formData})
+    setUser({logged})
+  }
+
+  const handleOnChange = (event) => {
+    const {name, value} = event.target
+    setFormData({...formData, [name]: value})
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    handleLogin()
+  }
 
   return (
     <div>
       <h1>Login</h1>
-      <button onClick={handleLogin}>Login</button>
+
+      <form onSubmit={handleSubmit} noValidate autoComplete="off">
+        <Grid container direction="column" justify="center" spacing={2}>
+          <Grid item>
+            <TextField id="user" label="User" name="user" variant="outlined" onChange={handleOnChange} />
+          </Grid>
+          <Grid item>
+            <TextField id="password" label="Password" name="password" variant="outlined" onChange={handleOnChange} />
+          </Grid>
+          <Grid item>
+            <Button color="primary" type="submit" variant="contained">
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
     </div>
   )
 }
---
+++ src/services/auth.js
@@ -0,0 +1,1 @@
+export const callToLoginEndpoint = () => ({logged: true})
